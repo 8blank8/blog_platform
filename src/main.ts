e54bea15ce252d090f3e1ed082bfc352da9b9115
8blank <roc.32@yandex.ru>
2023-07-26 19:56:04 +0700

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exception.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+  app.useGlobalPipes(new ValidationPipe({
+    stopAtFirstError: true,
+    exceptionFactory: (errors) => {
+      console.log(errors)
+      const newArr = errors.map(({ property, constraints }) => {
+        console.log(Object.keys(constraints))
+        return {
+          message: 'x',
+          field: property
+        }
+      })
+    },
+  }))
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(5000);
 }
 bootstrap();
