26784e1b26748d16e1efdbf7af31412c6deb5b7e
8blank8 <roc.32@yandex.ru>
2023-11-19 19:52:48 +0700

    fix: add bonus for addAnswerUseCase

--- src/features/quiz/application/useCases/add.answer.use.case.ts
+++ src/features/quiz/application/useCases/add.answer.use.case.ts
@@ -6,6 +6,7 @@ import { QuizRepositoryTypeorm } from '@quiz/repository/typeorm/quiz.repository.
 import { Answer } from '@quiz/domain/typeorm/answer.entity';
 import { Game } from '@quiz/domain/typeorm/quiz.game';
 import { QuizPlayer } from '@quiz/domain/typeorm/quiz.player.entity';
+import { QuizScore } from '@quiz/domain/typeorm/quiz.score.entity';
 
 export class AddAnswerCommand {
   constructor(public inputData: AnswerCreateModel, public userId: string) { }
@@ -49,7 +50,7 @@ export class AddAnswerUseCase {
 
     await this.quizRepository.saveGame(game);
 
-    await this.checkExpiresGame(game.id, player.id)
+    await this.checkExpiresGame(game.id, player)
 
 
     return answer.id;
@@ -65,28 +66,24 @@ export class AddAnswerUseCase {
 
     for (let i = answerCount.length; i < 5; i++) {
       await this.addAnswer(game, player, null, i)
-      console.log('add failed answer')
     }
   }
 
-  private async checkExpiresGame(gameId: string, playerId: string) {
+  private async checkExpiresGame(gameId: string, player: QuizPlayer) {
     const game = await this.quizQueryRepository.findFullGameByGameId(gameId)
     if (!game || !game.answer) return false
 
-    const answers = game.answer.filter(answer => answer.userId === playerId)
-    console.log(answers, 'before start timeout')
+    const answers = game.answer.filter(answer => answer.userId === player.id)
     if (answers.length === 5) {
       setTimeout(async () => {
-        console.log('start timeout')
-        await this.addFiledAswers(game, playerId)
-        await this.checkFinishGame(game)
+        await this.addFiledAswers(game, player.id)
+        await this.checkFinishGame(game, player.id)
         await this.quizRepository.saveGame(game)
       }, 10000);
     }
   }
 
   private async addStatistic(game: Game, playerId: string) {
-    console.log(`add statistic ${playerId}`)
     const player = await this.quizQueryRepository.findPlayerByPlayerId(playerId)
     if (!player) return false
 
@@ -167,27 +164,32 @@ export class AddAnswerUseCase {
       : game.firstPlayer.id;
   }
 
-  private async checkFinishGame(game: Game) {
+  private async checkFinishGame(game: Game, playerId: string = '') {
     const isFinished = game.answer.length === 10 ? true : false;
     if (isFinished) {
-      console.log('check finish game')
       game.finishGameDate = new Date().toISOString();
       game.status = 'Finished';
 
-      await this.addBonus(game);
+      await this.addBonus(game, playerId);
 
       await this.addStatistic(game, game.secondPlayer.id)
       await this.addStatistic(game, game.firstPlayer.id)
     }
   }
 
-  private async addBonus(game: Game) {
-    console.log('add bonus')
+  private async addBonus(game: Game, playerId: string = '') {
     // TODO: исправить начисления бонусных очков
     const answer = game.answer.find(answer => answer.answerStatus === 'Correct')
     if (!answer) return false
 
-    const score = await this.quizQueryRepository.findPlayerScoreByUserId(game.id, answer.userId)
+    let score: QuizScore | null
+    if (playerId.length) {
+      score = await this.quizQueryRepository.findPlayerScoreByUserId(game.id, playerId)
+      console.log('playerId is found')
+    } else {
+      score = await this.quizQueryRepository.findPlayerScoreByUserId(game.id, answer.userId)
+    }
+
     if (score) {
       score.score += 1
       await this.quizRepository.saveScore(score)
