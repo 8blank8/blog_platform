da80af448ae3d8c88138326cd867c4ea3675953e
8blank8 <roc.32@yandex.ru>
2023-11-21 19:12:22 +0700

    add method: findAllBlogsUserView for blogQueryRepo

--- src/features/blog/infrastructure/typeorm/blog.query.repository.typeorm.ts
+++ src/features/blog/infrastructure/typeorm/blog.query.repository.typeorm.ts
@@ -9,13 +9,51 @@ import { Repository } from 'typeorm';
 export class BlogQueryRepositoryTypeorm {
   constructor(
     @InjectRepository(Blogs) private blogRepository: Repository<Blogs>,
-  ) {}
+  ) { }
 
   async findBlogViewById(blogId: string): Promise<Blogs | null> {
     return this.blogRepository
       .createQueryBuilder('b')
       .where('id = :blogId', { blogId })
       .getOne();
+  }
+
+  async findAllBlogsUserView(queryParam: BlogQueryParamModel, userId: string) {
+    const pagination = new BlogPagination(queryParam).getBlogPaginationForSql();
+    const {
+      searchNameTerm,
+      sortBy,
+      sortDirection,
+      pageNumber,
+      pageSize,
+      offset,
+    } = pagination;
+
+    const blogs = await this.blogRepository
+      .createQueryBuilder('b')
+      .where('name ILIKE :searchNameTerm', { searchNameTerm })
+      .andWhere('b."userId" = :userId', { userId })
+      .orderBy(
+        `"${sortBy}" ${sortBy === 'createdAt' ? '' : 'COLLATE "C"'}`,
+        sortDirection,
+      )
+      .offset(offset)
+      .limit(pageSize)
+      .getMany();
+
+    const totalCount = await this.blogRepository
+      .createQueryBuilder('b')
+      .where('name ILIKE :searchNameTerm', { searchNameTerm })
+      .andWhere('b."userId" = :userId', { userId })
+      .getCount();
+
+    return {
+      pagesCount: Math.ceil(totalCount / pageSize),
+      page: pageNumber,
+      pageSize: pageSize,
+      totalCount: totalCount,
+      items: blogs,
+    };
   }
 
   async findAllBlogsView(queryParam: BlogQueryParamModel) {
@@ -52,5 +90,14 @@ export class BlogQueryRepositoryTypeorm {
       totalCount: totalCount,
       items: blogs,
     };
+  }
+
+  async findFullBlogById(blogId: string): Promise<Blogs | null> {
+    const blog = await this.blogRepository.createQueryBuilder('b')
+      .where('b.id = :blogId', { blogId })
+      .leftJoinAndSelect('b.user', 'u')
+      .getOne()
+
+    return blog
   }
 }
