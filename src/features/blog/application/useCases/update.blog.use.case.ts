de2e23f49b008d1077c02af0c11b2edf10bff0f6
8blank8 <roc.32@yandex.ru>
2023-11-21 19:11:06 +0700

    feat: check forbidden for updateBlogUseCase

--- src/features/blog/application/useCases/update.blog.use.case.ts
+++ src/features/blog/application/useCases/update.blog.use.case.ts
@@ -2,9 +2,14 @@ import { CommandHandler } from '@nestjs/cqrs';
 import { BlogUpdateType } from '@blog/models/blog.update.type';
 import { BlogRepositoryTypeorm } from '@blog/repository/typeorm/blog.repository.typeorm';
 import { BlogQueryRepositoryTypeorm } from '@blog/repository/typeorm/blog.query.repository.typeorm';
+import { ForbiddenException } from '@nestjs/common';
 
 export class UpdateBlogCommand {
-  constructor(public updateData: BlogUpdateType, public id: string) {}
+  constructor(
+    public updateData: BlogUpdateType,
+    public id: string,
+    public userId: string
+  ) { }
 }
 
 @CommandHandler(UpdateBlogCommand)
@@ -12,13 +17,14 @@ export class UpdateBlogUseCase {
   constructor(
     private blogRepository: BlogRepositoryTypeorm,
     private blogQueryRepository: BlogQueryRepositoryTypeorm,
-  ) {}
+  ) { }
 
   async execute(command: UpdateBlogCommand) {
-    const { updateData, id } = command;
+    const { updateData, id, userId } = command;
 
-    const blog = await this.blogQueryRepository.findBlogViewById(id);
+    const blog = await this.blogQueryRepository.findFullBlogById(id);
     if (!blog) return false;
+    if (blog.user.id !== userId) throw new ForbiddenException()
 
     blog.name = updateData.name;
     blog.description = updateData.description;
