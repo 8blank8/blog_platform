9ca798115e72a11615c619688cb0149207621d18
8blank8 <roc.32@yandex.ru>
2023-11-21 19:16:23 +0700

    feat: add imports for SaModule

--- src/features/sa/sa.module.ts
+++ src/features/sa/sa.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 
-import { SaQueryRepository } from './infrastructure/sa.query.repository';
 import { SaBlogController } from './api/sa.blog.controller';
+import { UserModule } from '@user/user.module';
+import { BlogModule } from '@blog/blog.module';
+import { CqrsModule } from '@nestjs/cqrs';
+import { SaQueryRepositoryTypeorm } from './infrastructure/sa.query.repository.typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Blogs } from '@blog/domain/typeorm/blog.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Blogs]),
+    UserModule,
+    BlogModule,
+    CqrsModule
+  ],
   controllers: [SaBlogController],
-  providers: [SaQueryRepository],
+  providers: [SaQueryRepositoryTypeorm],
 })
-export class SaModule {}
+export class SaModule { }
