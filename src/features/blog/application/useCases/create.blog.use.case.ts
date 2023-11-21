5e1a51910fc6815c07b718993b5df32d1316c37d
8blank8 <roc.32@yandex.ru>
2023-11-21 19:09:15 +0700

    feat: check forbidden

--- src/features/blog/application/useCases/create.blog.use.case.ts
+++ src/features/blog/application/useCases/create.blog.use.case.ts
@@ -2,22 +2,33 @@ import { CommandHandler } from '@nestjs/cqrs';
 import { BlogCreateType } from '@blog/models/blog.create.type';
 import { Blogs } from '@blog/domain/typeorm/blog.entity';
 import { BlogRepositoryTypeorm } from '@blog/repository/typeorm/blog.repository.typeorm';
+import { UserQueryRepositoryTypeorm } from '@user/repository/typeorm/user.query.repository.typeorm';
 
 export class CreateBlogCommand {
-  constructor(public blog: BlogCreateType) {}
+  constructor(
+    public blog: BlogCreateType,
+    public userId: string
+  ) { }
 }
 
 @CommandHandler(CreateBlogCommand)
 export class CreateBlogUseCase {
-  constructor(private blogRepository: BlogRepositoryTypeorm) {}
+  constructor(
+    private blogRepository: BlogRepositoryTypeorm,
+    private userQueryRepository: UserQueryRepositoryTypeorm
+  ) { }
 
-  async execute(command: CreateBlogCommand): Promise<string> {
-    const { blog } = command;
+  async execute(command: CreateBlogCommand): Promise<string | boolean> {
+    const { blog, userId } = command;
+
+    const user = await this.userQueryRepository.findUserByIdForSa(userId)
+    if (!user) return false
 
     const createdBlog = new Blogs();
     createdBlog.name = blog.name;
     createdBlog.description = blog.description;
     createdBlog.websiteUrl = blog.websiteUrl;
+    createdBlog.user = user
 
     await this.blogRepository.saveBlog(createdBlog);
     return createdBlog.id;
