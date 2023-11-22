8a3e8da450b920e5e27cb0a1a8a1ecc2f11fce19
8blank8 <roc.32@yandex.ru>
2023-11-22 20:40:00 +0700

    feat: update BanUserForBlogUseCase in typeorm

--- src/features/blog/application/useCases/ban.user.for.blog.use.case.ts
+++ src/features/blog/application/useCases/ban.user.for.blog.use.case.ts
@@ -1,59 +1,59 @@
 import { CommandHandler } from '@nestjs/cqrs';
 import { ForbiddenException } from '@nestjs/common';
-import { UserQueryRepositorySql } from '@user/repository/sql/user.query.repository.sql';
 import { BanUserForBlogModel } from '@blog/models/ban.user.for.blog.model';
-import { BlogQueryRepositorySql } from '@blog/repository/sql/blog.query.repository.sql';
-import { UserBanBlogQueryRepositorySql } from '@blog/repository/sql/user.ban.blog.query.repository.sql';
-import { UserBanBlogRepositorySql } from '@blog/repository/sql/user.ban.blog.repository.sql';
 import { BannedUserForBlogCreateSqlModel } from '@blog/models/banned.user.for.blog.create.sql.model';
+import { UserQueryRepositoryTypeorm } from '@user/repository/typeorm/user.query.repository.typeorm';
+import { BlogQueryRepositoryTypeorm } from '@blog/repository/typeorm/blog.query.repository.typeorm';
+import { UserBlogBanRepositoryTypeorm } from '@blog/repository/typeorm/user.ban.blog.repository';
+import { UserBlogBanQueryRepositoryTypeorm } from '@blog/repository/typeorm/user.ban.blog.query.repository';
+import { BlogBanUser } from '@blog/domain/typeorm/blog.ban.user.entity';
 
 export class BanUserForBlogCommand {
   constructor(
     public inputData: BanUserForBlogModel,
     public bannedUserId: string,
     public blogUserId: string,
-  ) {}
+  ) { }
 }
 
 @CommandHandler(BanUserForBlogCommand)
 export class BanUserForBlogUseCase {
   constructor(
-    private userQueryRepositorySql: UserQueryRepositorySql,
-    private blogQueryRepositorySql: BlogQueryRepositorySql,
-    private userBanBlogQueryRepositorySql: UserBanBlogQueryRepositorySql,
-    private userBanBlogRepositorySql: UserBanBlogRepositorySql,
-  ) {}
+    private userQueryRepository: UserQueryRepositoryTypeorm,
+    private blogQueryRepository: BlogQueryRepositoryTypeorm,
+    private userBanBlogQueryRepository: UserBlogBanQueryRepositoryTypeorm,
+    private userBanBlogRepository: UserBlogBanRepositoryTypeorm,
+  ) { }
 
   async execute(command: BanUserForBlogCommand): Promise<boolean> {
     const { inputData, bannedUserId, blogUserId } = command;
 
-    const user = await this.userQueryRepositorySql.findUser(bannedUserId);
+    const user = await this.userQueryRepository.findUserByIdForSa(bannedUserId);
     if (!user) return false;
 
-    const blog = await this.blogQueryRepositorySql.findBlogFullById(
+    const blog = await this.blogQueryRepository.findFullBlogById(
       inputData.blogId,
     );
     if (!blog) return false;
-    if (blog.userId !== blogUserId) throw new ForbiddenException();
+    if (blog.user.id !== blogUserId) throw new ForbiddenException();
 
-    const bannedUser = await this.userBanBlogQueryRepositorySql.findBannedUser(
+    let bannedUser = await this.userBanBlogQueryRepository.findBannedUserForBlog(
       user.id,
       blog.id,
     );
 
-    const updateData: BannedUserForBlogCreateSqlModel = {
-      userId: user.id,
-      blogId: blog.id,
-      isBanned: inputData.isBanned,
-      banReason: inputData.banReason,
-    };
-
     if (!bannedUser) {
-      await this.userBanBlogRepositorySql.createBanUserForBlogger(updateData);
-      return true;
+      bannedUser = new BlogBanUser()
     }
 
-    await this.userBanBlogRepositorySql.updateBanStatus(updateData);
-    return true;
+    bannedUser.banDate = inputData.isBanned ? new Date().toISOString() : null
+    bannedUser.banReason = inputData.isBanned ? inputData.banReason : null
+    bannedUser.blog = blog
+    bannedUser.isBanned = inputData.isBanned
+    bannedUser.user = user
+
+    await this.userBanBlogRepository.saveBlogBan(bannedUser)
+
+    return true
   }
 }
