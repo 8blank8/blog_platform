d69be3f9eac8957697067f1df3c83de9e64a6eb0
8blank8 <roc.32@yandex.ru>
2023-11-28 23:40:14 +0700

    add file: CreateTelegramAuthCodeUseCase

---
+++ src/features/integrations/useCases/create.telegram.auth.code.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { UserQueryRepositoryTypeorm } from "@user/repository/typeorm/user.query.repository.typeorm";
+import { UserRepositoryTypeorm } from "@user/repository/typeorm/user.repository.typeorm";
+import { v4 as uuidv4 } from 'uuid'
+
+
+@Injectable()
+export class CreateTelegramAuthCodeUseCase {
+    constructor(
+        private userQueryRepository: UserQueryRepositoryTypeorm,
+        private userRepository: UserRepositoryTypeorm
+    ) { }
+
+    async execute(userId: string): Promise<string | boolean> {
+
+        const user = await this.userQueryRepository.findUserByIdForSa(userId)
+        const profile = await this.userQueryRepository.findTelegramProfileByUserId(userId)
+        if (!user || profile) return false
+
+        const code = uuidv4()
+
+        user.telegramCode = code
+
+        await this.userRepository.saveUser(user)
+
+        return `https://t.me/blog_patf_bot?start=code=${code}`
+    }
+}
