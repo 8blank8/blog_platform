a0dcb3b58a68770c70fd437d2f048acae2ba661b
8blank8 <roc.32@yandex.ru>
2023-11-28 23:42:37 +0700

    add file: TelegramAdapter

---
+++ src/utils/adapters/telegram.adapter.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import axios from 'axios';
+import { AxiosInstance } from "axios";
+
+
+@Injectable()
+export class TelegramAdapter {
+    axiosInstance: AxiosInstance;
+    constructor() {
+        this.axiosInstance = axios.create({
+            baseURL: `https://api.telegram.org/bot${String(process.env.TELEGRAM_KEY)}/`
+        })
+    }
+
+    async setWebhook(baseUrl: string) {
+        await this.axiosInstance.post('setWebhook', {
+            url: `${baseUrl}/integrations/telegram/webhook`
+        })
+    }
+
+    async sendMessage(telegramId: number, text: string) {
+        await this.axiosInstance.post('sendMessage', {
+            chat_id: telegramId,
+            text: text
+        })
+    }
+
+}
