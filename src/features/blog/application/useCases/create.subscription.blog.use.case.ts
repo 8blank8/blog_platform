4bbd3263fdf4ee1b6e478fdc5b106447f0a51edb
8blank8 <roc.32@yandex.ru>
2023-11-28 23:38:15 +0700

    add file: CreateSubscriptionBlogUseCase

---
+++ src/features/blog/application/useCases/create.subscription.blog.use.case.ts
@@ -0,0 +1,42 @@
+import { BlogSubscription } from "@blog/domain/typeorm/blog.subscription";
+import { BlogQueryRepositoryTypeorm } from "@blog/repository/typeorm/blog.query.repository.typeorm";
+import { BlogRepositoryTypeorm } from "@blog/repository/typeorm/blog.repository.typeorm";
+import { CommandHandler } from "@nestjs/cqrs";
+import { UserQueryRepositoryTypeorm } from "@user/repository/typeorm/user.query.repository.typeorm";
+
+
+export class CreateSubscriptionBlogCommand {
+    constructor(
+        public userId: string,
+        public blogId: string
+    ) { }
+}
+
+@CommandHandler(CreateSubscriptionBlogCommand)
+export class CreateSubscriptionBlogUseCase {
+    constructor(
+        private userQueryRepository: UserQueryRepositoryTypeorm,
+        private blogQueryRepository: BlogQueryRepositoryTypeorm,
+        private blogRepository: BlogRepositoryTypeorm
+    ) { }
+
+    async execute(command: CreateSubscriptionBlogCommand): Promise<boolean> {
+        const { userId, blogId } = command
+
+        const user = await this.userQueryRepository.findUserByIdForSa(userId)
+        const blog = await this.blogQueryRepository.findFullBlogById(blogId)
+        if (!user || !blog) return false
+
+        const subscription = new BlogSubscription()
+        subscription.blog = blog
+        subscription.user = user
+        subscription.currentUserSubscriptionStatus = 'Subscribed'
+
+        blog.subscribersCount += 1
+
+        await this.blogRepository.saveBlog(blog)
+        await this.blogRepository.saveSubscription(subscription)
+
+        return true
+    }
+}
