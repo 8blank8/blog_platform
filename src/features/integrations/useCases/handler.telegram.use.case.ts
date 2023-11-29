87a0ef89fea4cfb795f20b9769fa26fd6d034ca0
8blank8 <roc.32@yandex.ru>
2023-11-29 23:38:57 +0700

    feat: fix authCode

--- src/features/integrations/useCases/handler.telegram.use.case.ts
+++ src/features/integrations/useCases/handler.telegram.use.case.ts
@@ -1,21 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { TelegramResponseModel } from "../models/telegram.response.model";
 import { CreateTelegramProfileUseCase } from "./create.telegram.profile.use.case";
+import { TelegramAdapter } from "@utils/adapters/telegram.adapter";
 
 @Injectable()
 export class HandlerTelegramUseCase {
 
     constructor(
-        private createTelegramProfileUseCase: CreateTelegramProfileUseCase
+        private createTelegramProfileUseCase: CreateTelegramProfileUseCase,
+        private telegramAdapter: TelegramAdapter
     ) { }
 
     async execute(inputData: TelegramResponseModel): Promise<boolean> {
-
-        const [other, code] = inputData.message.text.split('=')
-
-        if (code) {
-            await this.createTelegramProfileUseCase.execute(code, inputData)
+        let authCode: string | null = null
+        if (inputData.message) {
+            const [other, code] = inputData.message.text.split('=')
+            authCode = code
         }
+        // console.log(inputData)
+        if (authCode) {
+            await this.createTelegramProfileUseCase.execute(authCode, inputData)
+        }
+        // else {
+        //     await this.telegramAdapter.sendMessage(String(inputData.message.from.id), 'Сори не знаю что это')
+        // }
 
         return true
     }
